6df3cc573c5105719c3d6eb0cbae5b611fe8d907
Matthew Serre <matthew.serre@dispatchit.com>
2023-03-14 23:00:30 -0400

    Add logic for sending lock and unlock requests to the server

--- cmd/bluelink-cli/main.go
+++ cmd/bluelink-cli/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"golang.org/x/crypto/ssh/terminal"
 
@@ -14,6 +15,7 @@ import (
 
 	authv1 "github.com/MatthewSerre/hyundai-bluelink-protobufs/gen/go/protos/authentication/v1"
 	infov1 "github.com/MatthewSerre/hyundai-bluelink-protobufs/gen/go/protos/information/v1"
+	remote_actionv1 "github.com/MatthewSerre/hyundai-bluelink-protobufs/gen/go/protos/remote_action/v1"
 	"github.com/joho/godotenv"
 )
 
@@ -31,8 +33,15 @@ type Vehicle struct {
 	Mileage string
 }
 
+type RemoteActionResponse struct {
+	Result string
+	FailMsg string
+	ResponseString *remote_actionv1.ResponseString
+}
+
 const Authentication_Address = "localhost:50051"
 const Information_Address = "localhost:50052"
+const Remote_Action_Address = "localhost:50053"
 
 func main() {
 	log.Println("Welcome to the unofficial Hyundai Bluelink CLI!")
@@ -50,8 +59,6 @@ func main() {
 	defer authConn.Close()
 
 	c := authv1.NewAuthenticationServiceClient(authConn)
-
-	log.Println("Connection established!")
 
 	var authStub Auth;
 
@@ -90,10 +97,8 @@ func main() {
 
 	d := infov1.NewInformationServiceClient(infoConn)
 
-	log.Println("Connection established!")
-
 	log.Println("Obtaining vehicle information...")
-
+		
 	info, err := getVehicleInfo(d, authStub)
 
 	if err != nil {
@@ -101,10 +106,55 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.Println("Vehicle information:")
-	log.Println("Registration ID:", info.RegistrationID)
-	log.Println("VIN:", info.VIN)
-	log.Println("Mileage:", info.Mileage)
+	remoteActionConn, err := grpc.Dial(Remote_Action_Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	e := remote_actionv1.NewRemoteActionServiceClient(remoteActionConn)
+
+	exitMenu := false;
+	for !exitMenu {
+		var command string
+		log.Println("Select an option from the list below and enter the corresponding number. Otherwise, enter 'exit' to close the application.")
+		log.Println("1. Display vehicle registration ID, VIN, and mileage)")
+		log.Println("2. Lock vehicle")
+		log.Println("3. Unlock vehicle")
+		fmt.Scan(&command)
+		switch command {
+		case "1":
+			log.Println("Registration ID:", info.RegistrationID)
+			log.Println("VIN:", info.VIN)
+			log.Println("Mileage:", info.Mileage)
+		case "2":
+			_, err := toggleLock(e, authStub, info, remote_actionv1.LockAction_LOCK_ACTION_LOCK)
+
+			if err != nil {
+				log.Println("vehicle lock request failed with error:", err)
+			}
+
+			if err == nil {
+				log.Println("No error returned from the server. Sleeping for three minutes to buffer requests...")
+				time.Sleep(180 * time.Second)
+				log.Println("Vehicle locked!")
+			}
+		case "3":
+			_, err := toggleLock(e, authStub, info, remote_actionv1.LockAction_LOCK_ACTION_UNLOCK)
+
+			if err != nil {
+				log.Println("vehicle unlock request failed with error:", err)
+			}
+
+			if err == nil {
+				log.Println("No error returned from the server. Sleeping for three minutes to buffer requests...")
+				time.Sleep(180 * time.Second)
+				log.Println("Vehicle unlocked!")
+			}
+		case "exit":
+			exitMenu = true
+		default:
+			continue
+		}
+	}
+
+	fmt.Println("Thank you for using the unofficial Hyundai Bluelink CLI!")
 }
 
 
@@ -164,8 +214,6 @@ func getInput(message string) (input string) {
 }
 
 func getVehicleInfo(c infov1.InformationServiceClient, auth Auth) (Vehicle, error) {
-	log.Println("GetVehicleInfo was invoked")
-
 	res, err := c.GetVehicleInfo(context.Background(), &infov1.VehicleInfoRequest{
 		Username: auth.Username,
 		Pin: auth.PIN,
@@ -178,4 +226,22 @@ func getVehicleInfo(c infov1.InformationServiceClient, auth Auth) (Vehicle, erro
 	}
 
 	return Vehicle{ RegistrationID: res.RegistrationId, VIN: res.Vin, Generation: res.Generation, Mileage: res.Mileage }, nil
+}
+
+func toggleLock(c remote_actionv1.RemoteActionServiceClient, auth Auth, vehicle Vehicle, lockAction remote_actionv1.LockAction) (RemoteActionResponse, error) {
+	res, err := c.ToggleLock(context.Background(), &remote_actionv1.ToggleLockRequest{
+		Username: auth.Username,
+		Pin: auth.PIN,
+		JwtToken: auth.JWT_Token,
+		RegistrationId: vehicle.RegistrationID,
+		Vin: vehicle.VIN,
+		Generation: vehicle.Generation,
+		LockAction: lockAction,
+	})
+
+	if err != nil {
+		return RemoteActionResponse{}, err
+	}
+
+	return RemoteActionResponse{Result: res.Result, FailMsg: res.FailMsg, ResponseString: res.ResponseString}, nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/MatthewSerre/hyundai-bluelink-cli
 go 1.20
 
 require (
-	github.com/MatthewSerre/hyundai-bluelink-protobufs v0.0.0-20230314210524-36482bed1291
+	github.com/MatthewSerre/hyundai-bluelink-protobufs v0.0.0-20230314231352-72e67f81a70e
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.7.0
 	google.golang.org/grpc v1.53.0
